a14e24a90f2411992379f7a1b2e015e4a64db739
jiwon <jiwon.=hur6@gmail.com>
2024-05-23 20:36:43 -0700

    img update

--- src/menu.js
+++ src/menu.js
@@ -2,7 +2,7 @@ export function menuLoad() {
   const content = document.getElementById("content");
 
   const gourmet_menu_img = document.createElement("img");
-  gourmet_menu_img.src = 'imgs/gourmet_menu.png';
+  gourmet_menu_img.src = './/imgs/gourmet_menu.png';
 
   const little_chef_menu_img = document.createElement("img");
   little_chef_menu_img.src = 'imgs/little_chef_menu.png';
